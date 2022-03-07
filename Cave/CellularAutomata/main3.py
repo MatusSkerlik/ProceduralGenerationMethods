6e6a88c470f1e1459ff2866dccbc3f6eeb4f386e
Matúš Škerlík <matus.skerlik@student.tuke.sk>
2022-03-07 18:33:25 +0100

    + refactor, wall durability

--- Cave/CellularAutomata/main3.py
+++ Cave/CellularAutomata/main3.py
@@ -12,14 +12,30 @@ from typing import List
 import pygame
 
 # global params
-width, height = 300, 40
+width, height = 100, 40
 
 class Cell:
-    def __init__(self, v = 0, c = 0):
+    def __init__(self, v = 0):
         self.v = v
-        self.c = c
+
+class Acid(Cell):
+    def __init__(self, d=0):
+        super().__init__(2)
+        self.d = d # durability
         self.visited_points = set()
 
+    def __repr__(self):
+        return "Acid, d = %d" % self.d
+
+class Wall(Cell):
+    def __init__(self, d=1):
+        super().__init__(1)
+        assert d > 0
+        self.d = d 
+
+class Void(Cell):
+    def __init__(self):
+        super().__init__(0)
 
 class Point:
     def __init__(self, x: int, y: int):
@@ -72,39 +88,39 @@ def neumann_nbs(x: int, y: int, Map: List[Cell]):
     return top, right, bottom, left
 
 def step(Map: List[Cell], Points: List[Point]):
-    Map_copy = [Cell() for _ in range(width * height)]
+    Map_copy = copy(Map) 
     for x in range(width):
         for y in range(height):
             cell = copy(Map[encode_coords(x, y)])
-            if cell.v == 0:
+            if isinstance(cell, Void):
                 continue
-            # find closest point
-            p_vector = Point(0, 0)
-            min_d = int(width * height)
-            for p in Points:
-                if cell.v == 2 and p.x == x and p.y == y:
-                    cell.visited_points.add(p)
-                if p not in cell.visited_points:
-                    p_v = Point(x - p.x, y - p.y) 
-                    d = math.sqrt((p_v.x ** 2) + (p_v.y ** 2))
-                    if cell.v == 2 and d < 3:
-                        cell.visited_points.add(p)
-                    if d < min_d:
-                        min_d = d
-                        p_vector = p_v
+            elif isinstance(cell, Acid):
+                # find closest point
+                p_vector = Point(0, 0)
+                min_d = int(width * height)
+                for p in Points:
+                    if p not in cell.visited_points:
+                        if p.x == x and p.y == y:
+                            cell.visited_points.add(p)
+                        else:
+                            p_v = Point(x - p.x, y - p.y) 
+                            d = math.sqrt((p_v.x ** 2) + (p_v.y ** 2))
+                            if d < 3:
+                                cell.visited_points.add(p)
+                            elif d < min_d:
+                                min_d = d
+                                p_vector = p_v
 
-            if cell.v == 2:
-                if len(cell.visited_points) == len(Points):
-                    cell.c = 0
-                    cell.v = 0
-
-                if cell.c > 0:
-                    # 0 = up, 1 = left, 2 = down, 3 = right
+                if (len(cell.visited_points) == len(Points)) or (cell.d <= 0):
+                    cell = Void()
+                    Map_copy[encode_coords(x, y)] = cell 
+                else: 
+                    # 0 = up, 1 = right, 2 = down, 3 = left 
                     dirs = []
                     if (p_vector.x > 0):
-                        dirs += [1 for _ in range(int(p_vector.x))]
+                        dirs += [3 for _ in range(int(p_vector.x))]
                     else:
-                        dirs += [3 for _ in range(int(-p_vector.x))]
+                        dirs += [1 for _ in range(int(-p_vector.x))]
 
                     if (p_vector.y > 0):
                         dirs += [0 for _ in range(int(p_vector.y))]
@@ -112,54 +128,32 @@ def step(Map: List[Cell], Points: List[Point]):
                         dirs += [2 for _ in range(int(-p_vector.y))]
                     direction = random.choice(dirs)
 
-                    up, right, down, left = neumann_nbs(x, y, Map)
-                    if direction == 0 and up is not None and up.v !=2:
-                        cell.v = 0
-                        if up.v == 1:
-                            up.c = cell.c - 1
+                    nbs = neumann_nbs(x, y, Map)
+                    nb = nbs[direction] 
+                    if not isinstance(nb, Acid):
+                        if isinstance(nb, Wall):
+                            if nb.d > 1:
+                                nb = Wall(nb.d - 1)
+                            else:
+                                if cell.d > 1:
+                                    nb = Acid(cell.d - 1) 
+                                    nb.visited_points = cell.visited_points
+                                else:
+                                    nb = Void()
+                                cell = Void()
                         else:
-                            up.c = cell.c
-                        up.v = 2
-                        if len(cell.visited_points) > 0:
-                            up.visited_points.update(cell.visited_points)
-                        Map_copy[encode_coords(x, y - 1)] = up 
-                    elif direction == 1 and left is not None and left.v != 2: 
-                        cell.v = 0
-                        if left.v == 1:
-                            left.c = cell.c - 1
+                            nb = Acid(cell.d)
+                            nb.visited_points = cell.visited_points
+                            cell = Void()
+                        if direction == 0:
+                            Map_copy[encode_coords(x, y - 1)] = nb 
+                        elif direction == 1:
+                            Map_copy[encode_coords(x + 1, y)] = nb 
+                        elif direction == 2:
+                            Map_copy[encode_coords(x, y + 1)] = nb 
                         else:
-                            left.c = cell.c
-                        left.v = 2
-                        if len(cell.visited_points) > 0:
-                            left.visited_points.update(cell.visited_points)
-                        Map_copy[encode_coords(x - 1, y)] = left 
-                    elif direction == 2 and down is not None and down.v != 2:
-                        cell.v = 0
-                        if down.v == 1:
-                            down.c = cell.c - 1
-                        else:
-                            down.c = cell.c
-                        down.v = 2
-                        if len(cell.visited_points) > 0:
-                            down.visited_points.update(cell.visited_points)
-                        Map_copy[encode_coords(x, y + 1)] = down 
-                    elif direction == 3 and right is not None and right.v != 2:
-                        cell.v = 0
-                        if right.v == 1:
-                            right.c = cell.c - 1
-                        else:
-                            right.c = cell.c
-                        right.v = 2
-                        if len(cell.visited_points) > 0:
-                            right.visited_points.update(cell.visited_points)
-                        Map_copy[encode_coords(x + 1, y)] = right 
-                else:
-                    cell.v = 0
-
-            cell_ = Map_copy[encode_coords(x, y)]
-            if cell_.v != 2:
-                Map_copy[encode_coords(x, y)] = cell 
-
+                            Map_copy[encode_coords(x - 1, y)] = nb 
+                        Map_copy[encode_coords(x, y)] = cell 
     return Map_copy
 
 def smooth_step(Map: List[Cell]):
@@ -167,37 +161,38 @@ def smooth_step(Map: List[Cell]):
     for x in range(width):
         for y in range(height):
             cell = copy(Map[encode_coords(x, y)])
-            if cell.v == 0:
+            if isinstance(cell, Void):
                 continue
-            elif cell.v == 2:
+            elif isinstance(cell, Acid):
                 Map_copy[encode_coords(x, y)] = cell
                 continue
-
-            nbs = moore_nbs(x, y, Map)
-            alive = sum([1 for _ in nbs if _ is not None and _.v == 1])
-            if alive >= 4:
-                Map_copy[encode_coords(x, y)] = cell
+            else:
+                nbs = moore_nbs(x, y, Map)
+                alive = sum([1 for _ in nbs if _ is not None and isinstance(_, Wall)])
+                if alive >= 4:
+                    Map_copy[encode_coords(x, y)] = cell
     return Map_copy
 
 if __name__ == '__main__':
     # local params
-    scale = 6
-    font_size = 16
     running = True
+    scale = 8
+    font_size = 24 
+    wall_durability = 3
+    acid_durability = 15 
     Points = []
-    Map = [Cell(0, 0) for _ in range(width * height)]
-    for x in range(width):
-        for y in range(height): 
-            cell = Map[encode_coords(x, y)]  
-            cell.v = 1
+    Map = [Wall(wall_durability) for _ in range(width * height)]
 
     # initialization of pygame environment
     pygame.init()
-    screen = pygame.display.set_mode([width * scale, height * scale])
+    pygame.font.init()
+    screen = pygame.display.set_mode([width * scale, height * scale], 0 , 8)
     clock = pygame.time.Clock()
+    font = pygame.font.SysFont("courier", font_size, True)
 
     while running:
-        clock.tick(25)
+        clock.tick(60)
+        fps = int(clock.get_fps())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -205,11 +200,7 @@ if __name__ == '__main__':
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 elif event.key == pygame.K_r:
-                    Map = [Cell(0, 0) for _ in range(width * height)]
-                    for x in range(width):
-                        for y in range(height): 
-                            cell = Map[encode_coords(x, y)]  
-                            cell.v = 1
+                    Map = [Wall(wall_durability) for _ in range(width * height)]
                     Points = []
                 elif event.key == pygame.K_s:
                     pygame.image.save(screen, "img/%d.png" % time.time())
@@ -217,9 +208,7 @@ if __name__ == '__main__':
                     Map = smooth_step(Map)
                 elif event.key == pygame.K_n:
                     for x in range(width):
-                        cell = Map[encode_coords(x, 0)]
-                        cell.v = 2
-                        cell.c = 99999 
+                        Map[encode_coords(x, 0)] = Acid(acid_durability)
                 elif event.key == pygame.K_m:
                     for _ in range(random.randint(3, 6)):
                         x = random.randint(0, width - 1)
@@ -229,15 +218,14 @@ if __name__ == '__main__':
         left, center, right = pygame.mouse.get_pressed()
         mx, my = pygame.mouse.get_pos()
         if (left):
-            cell = Map[encode_coords(int(mx / scale), int(my / scale))]
-            cell.v = 2
-            cell.c = 99999 
+            Map[encode_coords(int(mx / scale), int(my / scale))] = Acid(acid_durability)
         if (right):
             Points.append(Point(int(mx / scale), int(my / scale)))
 
         Map = step(Map, Points)
 
         screen.fill((0, 0, 0))
+        
         for x in range(width):
             for y in range(height):
                 cell = Map[encode_coords(x, y)]
@@ -247,5 +235,8 @@ if __name__ == '__main__':
                     pygame.draw.rect(screen, color=(0, 255, 0), rect=(x * scale, y * scale, scale, scale))
         for p in Points:
             pygame.draw.circle(screen, (255, 0, 0), (p.x * scale, p.y * scale), 5)
+
+        fps_text = font.render(str(fps), True, (0, 255, 0)) 
+        screen.blit(fps_text, (0, 0))
         pygame.display.flip()
     pygame.quit()
